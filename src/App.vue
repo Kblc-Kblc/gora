f7e4b72381ce76002fb15d3e1912cb131f17ea94
Kblc-Kblc <75986241+Kblc-Kblc@users.noreply.github.com>
2021-04-26 16:33:04 +0300

    ++++

--- src/App.vue
+++ src/App.vue
@@ -62,8 +62,10 @@ body {
   overflow: hidden;
   display: flex;
   flex-direction: column;
-  background: url("./assets/1.svg") 100% no-repeat;
   height: 100vh;
+  background: url("./assets/1.svg") no-repeat;
+  background-size: 100% 100%;
+  background-attachment: fixed;
 
   @media (max-width: $md2+px) {
     overflow: auto;
